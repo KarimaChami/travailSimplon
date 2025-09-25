4f5868e27a3a81f3d2038b92c852676ea6cafd4b
KarimaChami <karimachami24@gmail.com>
2025-09-25 15:47:39 +0100

    new code

--- fonctions.py
+++ fonctions.py
@@ -1,41 +1,47 @@
 from db import produits,prix
 #Qst1
+
 combiner = list(zip(produits,prix))
 print(combiner)
+
 #Qst2
 def filter_fonction():
  filtrer = list(filter(lambda x:x >=30,prix))
- return filtrer
-print(filter_fonction())
+ print(filtrer) 
+
 
 # #Qst3
 def affichage_fonction():
  chaine = ""
  for p,pr in combiner :
    chaine += f"{p} coute {pr} DH\n"
- return chaine
-print(affichage_fonction())
+ print(chaine)
 
-#Qst4
+# #Qst4
 def sort_fonction():
     combiner.sort(key=lambda x:x[1])
-    return combiner
-print(sort_fonction())
+    print(combiner) 
 
-#Qst6
+# #Qst6
 def tuple_fonction():
-    return tuple(combiner)
-    print (tuple)
-tuple_fonction()
+    print (tuple(combiner))
 
-#Qst7
+
+# #Qst7
 def max_fonction():
   combiner.sort(key=lambda x:x[1],reverse=True)
-  print(combiner[0])
+  print(f"Le produit le plus cher{combiner[0]}")
 
 
 def min_fonction():
   combiner.sort(key=lambda x:x[1])
-  print(combiner[0])
-min_fonction()
-   
+  print(f"Le produit le moins cher{combiner[0]}")
+
+  
+#Qst8
+def bonus_fonction():
+ produit_luxe = list(map(lambda x: f"{x[0]} cout {x[1]} DH(LUXE)\n" 
+                         if x[1] > 1000 
+                         else f"{x[0]} coute {x[1]} DH",combiner))
+ print(produit_luxe)
+
--- travail.py
+++ travail.py
@@ -1,4 +1,10 @@
 from fonctions import *
 
+filter_fonction()
+affichage_fonction()
+sort_fonction()
+tuple_fonction()
+# combiner_fonction()
 max_fonction()
 min_fonction()
+bonus_fonction()
